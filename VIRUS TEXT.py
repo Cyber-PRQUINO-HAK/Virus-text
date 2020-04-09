6d18faf5f38f1f869182b0f23a4db674c16b1d39
H.A.Kvortex <57755112+Cyber-PRQUINO-HAK@users.noreply.github.com>
2020-04-09 12:15:41 +0900

    Add files via upload

---
+++ VIRUS TEXT.py
@@ -0,0 +1,14 @@
+
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai impian kamu
+#angka terkecil ialah yang paling cepat
+#angka terbesar ialah yang paling lambat
+mengetik('[########################################]\n Kalian loding dulu \n =>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\n→\n→\n→\nSELAMAT DATANG DI TOOLS PRQUINO\n......__________\n.....[>o<$_$>o<] \n.........[ΠΠΠΠ] \n.....(€€€€€€€)       WELCOME ™ \n..........(###) \n...........(###) \n...........(###) \n..........(###) \nDI BAWAH INI CARA BAGAIMANA CARA MEMBUKA VIRUS NYA \n 1.]nano 1.VIRUS GANAS.txt \n 2.]nano 2.VIRUS GANAS.txt \n 3.]nano 3.VIRUS GANAS.txt \n4.] nano 4.VIRUS GANAS\n5.] nano 5.VIRUS GANAS\n6.] nano 6.VIRUS GANAS\n7.] nano 7.VIRUS GANAS\n→\n→\nIDENTIFIKASI VIRUS \n→\nVirus text merupakan program komputer yang dapat menggandakan atau menyalin dirinya sendiri \n [1] dan menyebar dengan cara menyisipkan salinan dirinya ke dalam program atau dokumen lain.\n Virus komputer dapat dianalogikan dengan virus biologis yang menyebar dengan cara menyisipkan dirinya sendiri ke sel makhluk hidup. \n Virus komputer dapat merusak (misalnya dengan merusak data pada dokumen), membuat pengguna komputer merasa terganggu, maupun tidak menimbulkan efek sama sekali. \n→\n→\n CARA KERJA VIRUS TEXT \n→\nVirus text umumnya dapat merusak perangkat lunak komputer dan tidak dapat secara langsung merusak perangkat keras komputer tetapi dapat \nmengakibatkan kerusakan dengan cara memuat program yang memaksa over process ke perangkat tertentu. \n Efek negatif virus komputer adalah memperbanyak dirinya sendiri, yang membuat sumber daya pada komputer (seperti penggunaan memori) menjadi berkurang secara signifikan.\n Hampir 95% virus komputer berbasis sistem operasi Windows. \nSisanya menyerang Linux/GNU, Mac, FreeBSD, OS/2 IBM, dan Sun Operating System.\n Virus yang ganas akan merusak perangkat keras \n→\n→\nPENCEGAHAN \n→\nSerangan virus dapat dicegah atau ditanggulangi dengan menggunakan antivirus.\n Jenis perangkat lunak ini dapat juga mendeteksi dan menghapus virus komputer. \nVirus komputer ini dapat dihapus dengan basis data (database/ Signature-based detection), heuristik, atau peringkat dari program itu sendiri (Quantum).\n[########################################]\nSELALU SUPPROT TEAM PRQUINO\nSUBSCRIBE = https://www.youtube.com/channel/UC7V4xkAZm0bX6_taK_F0zhw \n instagram = prquino \n facebook = prquinoherlambang \n[########################################]\n terima kasih\n')
